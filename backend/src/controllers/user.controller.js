140b3585be88dc880060ac3f1c844d346384fb38
akramexp <akram.exp02@gmail.com>
2024-08-05 17:37:18 +0530

    changes in cookie domain

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -72,6 +72,7 @@ export const loginUser = asyncHandler(async (req, res) => {
       secure: true,
       sameSite: "None",
       path: "/",
+      domain: ".vercel.app",
     })
     .status(200)
     .json(
