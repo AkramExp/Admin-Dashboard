61e5a0f5466ce5d1dc393a60ff987ed6d4f98842
akramexp <akram.exp02@gmail.com>
2024-08-06 00:13:35 +0530

    changes

--- backend/src/middleware/multer.middleawre.js
+++ backend/src/middleware/multer.middleawre.js
@@ -1,5 +1,10 @@
 import multer from "multer";
 import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
