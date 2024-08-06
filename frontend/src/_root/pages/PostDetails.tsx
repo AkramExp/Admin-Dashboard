9792ef9d51b715fc931968a9b78c2ff058df59e7
akramexp <akram.exp02@gmail.com>
2024-08-06 23:16:36 +0530

    changes in post detail

--- frontend/src/_root/pages/PostDetails.tsx
+++ frontend/src/_root/pages/PostDetails.tsx
@@ -73,7 +73,7 @@ const PostDetails = () => {
                     "/assets/icons/profile-placeholder.svg"
                   }
                   alt="creator"
-                  className="rounded-full w-8 h-8 lg:w-12 lg:h-12"
+                  className="rounded-full w-8 h-8 lg:w-12 lg:h-12 object-cover"
                 />
 
                 <div className="flex flex-col">
